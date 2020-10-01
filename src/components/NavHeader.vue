575bc38b7c225a6d70d87febf065722eaf014c5b
lzl <1399669038@qq.com>
2020-10-01 22:24:42 +0800

    navheader-1

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-        nav-header
+    <div class="header">
+        <div class="nav-topbar">
+            <div class="container">
+                <div class="topbar-menu">
+                    <a href="javascript:;">小米商城</a>
+                    <a href="javascript:;">MIUI</a>
+                    <a href="javascript:;">云服务</a>
+                    <a href="javascript:;" >协议规则</a>
+                </div>
+                <div class="topbar-user">
+                    <a href="javascript:;">登录</a>
+                    <a href="javascript:;">注册</a>
+                    <a href="javascript:;" class="my-cart">
+                        <span class="icon-cart" ></span>
+                        购物车
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="nav-header"></div>
     </div>
 </template>
 <script>
@@ -8,3 +26,44 @@ export default {
     name:'nav-header'
 }
 </script>
+<style lang="scss">
+    .header{
+        .nav-topbar{
+            height: 39px;
+            line-height: 39px;
+            background-color: #333;
+            color: #b0b0b0;
+
+            .container{
+                min-width: 1226px;
+                margin: 0 auto;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                
+                a{
+                    display: inline-block;
+                    color: #b0b0b0;
+                    margin-right: 17px;
+                }
+                
+                .my-cart{
+                    width: 110px;
+                    background-color: #ff6600;
+                    text-align: center;
+                    color: #fff;
+
+                    .icon-cart{
+                        display: inline-block;
+                        width: 16px;
+                        height: 12px;
+                        background: url('../../public/imgs/icon-cart-checked.png') no-repeat center;
+                        background-size: contain;
+                        margin-right:4px;
+                    }
+                }
+            }
+        }
+        
+    }
+</style>
