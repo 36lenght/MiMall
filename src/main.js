67d07b8ee446b50015c3f10a559710cb21d74c21
lzl <1399669038@qq.com>
2020-10-12 10:35:11 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios' // 引入axios
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios  // 挂载axios
 
 
 new Vue({
